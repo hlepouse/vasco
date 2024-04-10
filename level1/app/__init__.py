534ce8fc746b65315e273586c2c59b5c9ce94926
Hadrien Lepousé <hlepouse@gmail.com>
2024-04-10 10:35:09 +0200

    [Level1] Make level test pass with basic app

--- level1/app/__init__.py
+++ level1/app/__init__.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
+def convert_percent_to_float(rate):
+
+    return rate / 100
+
+def preprocess_targets(json_file):
+
+    targets_list = json.load(json_file)
+    targets_dict = {}
+
+    for target in targets_list:
+
+        for rateKey in ["churnRate", "downgradeRate", "upgradeRate"]:
+            target[rateKey] = convert_percent_to_float(target[rateKey])
+
+        targets_dict[target["year"], target["month"]] = target
+
+    return targets_dict
 
 def create_app():
 
     app = Flask(__name__)
 
+    with open("data/targets.json") as json_file:
+
+        app.config['TARGETS'] = preprocess_targets(json_file)
+
     @app.route('/trpc/targets.perMonth')
     def targets_perMonth():
 
-        return {}
+        input = json.loads(request.args.get('input'))
+
+        return app.config['TARGETS'][input["year"], input["month"]]
 
     return app
